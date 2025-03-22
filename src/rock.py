97a21e39b0c08b517ca0f975450efc37fc78ac52
Daniel Lovick <daniel1.lovick@gmail.com>
2025-03-22 15:01:06 +0000

    Added skeleton for rock stage

---
+++ src/rock.py
@@ -0,0 +1,11 @@
+class rockStage():
+    def __init__(self):
+        self.rockWavePresent=False
+        self.rockWaveCount=0
+        self.rockWaveDirection=-1
+        
+    def draw(self,window):
+        window.fill("green")
+        if not self.rockWavePresent:
+            pass
+

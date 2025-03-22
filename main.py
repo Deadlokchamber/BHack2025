9c15061c2e1dc184743f7a0afa9184cdc9102a5f
Daniel Lovick <daniel1.lovick@gmail.com>
2025-03-22 12:45:09 +0000

    Added neuphonic game mode and tests

--- main.py
+++ main.py
@@ -4,9 +4,9 @@ pygame.init()
 
 screen = pygame.display.set_mode((1280,720))
 
-clunk = pygame.image.load_extended("Images/Clunk/clunk-forward.png")
-
 clock = pygame.time.Clock()
+
+neuphonicGameMode=True
 
 while True:
     # Process player inputs.
@@ -17,15 +17,12 @@ while True:
 
     # Do logical updates here.
     # ...
-
-    screen.fill("purple")  # Fill the display with a solid color
-
+    if not neuphonicGameMode:
+        screen.fill("purple")  # Fill the display with a solid color
+    if neuphonicGameMode:
+        screen.fill("red")
     # Render the graphics here.
     # ...
 
     pygame.display.flip()  # Refresh on-screen display
     clock.tick(60)         # wait until next frame (at 60 FPS)
-win = pygame.display.set_mode((1920,960))
-
-while True:
-    print("running")

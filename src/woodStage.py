2cc5c79f98fa27afba1a7d9adfafca864d0c441c
Deadlokchamber <sxd425@student.bham.ac.uk>
2025-03-23 11:21:13 +0000

    more maps

--- src/woodStage.py
+++ src/woodStage.py
@@ -1,3 +1,4 @@
+import random 
 import pygame as pg
 import sys
 from os import path
@@ -20,7 +21,8 @@ class WoodStage:
 
         
     def load_data(self):
-        self.map = Map('map3.txt')
+        self.mapno = random.randint(1, 6)
+        self.map = Map("map{}.txt".format(self.mapno*2))
         self.background = pg.image.load('../Images/treeback.png') 
         
 
